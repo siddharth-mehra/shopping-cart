8e6e6aa9a292c5a43ccbc68d2d6a7b8d1c867f1c
siddharth-mehra <sid15.mehra@gmail.com>
2024-11-11 17:35:05 +0530

    Updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,7 @@
 import {toast} from "react-hot-toast"
 import {useDispatch,useSelector} from "react-redux"
 import {add,remove} from "../redux/slices/CartSlice"
-import { MdImageSearch } from "react-icons/md";
+
 import axios from 'axios'
 import React,{ useCallback, useMemo } from "react";
 
@@ -62,10 +62,13 @@ const Product = React.memo(({ post }) => {
             </div>
             <div className="relative z-[0] w-full h-[180px] flex justify-center">
                 <img src={post.image} className="h-full" alt={post.title} />
-                <MdImageSearch
-                    onClick={() => searchSimilarImages(post.title)}
-                    className="absolute z-[999] h-[50px] w-[50px] bottom-10 left-0 bg-black text-white rounded-full px-3 py-1"
-                />
+                <svg viewBox="0 0 24 24" focusable="false" height="40" width="40" onClick={() => searchSimilarImages(post.title)}
+                   className="absolute z-50 bottom-2 left-2 text-black rounded-full p-1 cursor-pointer">
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M21,9v4h-2V9c0-1.1-0.9-2-2-2H7C5.9,7,5,7.9,5,9v3H3V9c0-2.21,1.79-4,4-4h2l1-2h4l1,2h2C19.21,5,21,6.79,21,9z M12,21H7 c-2.21,0-4-1.79-4-4v-2h2v2c0,1.1,0.9,2,2,2h5V21z M18,16c1.1,0,2,0.9,2,2s-0.9,2-2,2s-2-0.9-2-2S16.9,16,18,16z M12,10   c1.66,0,3,1.34,3,3s-1.34,3-3,3s-3-1.34-3-3S10.34,10,12,10z">
+                        </path>
+                </svg>
+                    
             </div>
             <div className="flex justify-between gap-12">
                 <div>
